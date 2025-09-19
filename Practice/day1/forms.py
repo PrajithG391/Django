9ccc0ff58620298f6d90b7a4ae29c953693737fd
PrajithG391 <kuttanthampuran3669@gmail.com>
2025-09-19 14:39:22 +0530

    auto commit - $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')

--- Practice/day1/forms.py
+++ Practice/day1/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField(label="Name", max_length=100)
     email = forms.EmailField(label="Email")
-    message = forms.CharField(widget=forms.Textarea, label="Message")  # Corrected here
+    message = forms.CharField(widget=forms.Textarea, label="Message")  
 
